1c987dfe91f37fe8fe413ae5859e9e28a5792b69
RaiderSlavo <matteoanejad@gmail.com>
2023-05-24 16:43:04 +0200

    first ui implementation

--- view/configurations/tabs/option/options.py
+++ view/configurations/tabs/option/options.py
@@ -5,122 +5,106 @@
 # Copyright (c) 2023 FIT-Project
 # SPDX-License-Identifier: GPL-3.0-only
 # -----
-######  
-
-from PyQt6 import QtCore, QtGui, QtWidgets
-
-from controller.configurations.tabs.general.network import Network as NetworkController
-
-__is_tab__ = False
-
-class Network(QtWidgets.QGroupBox):
-
-  def __init__(self, parent=None):
-
-    super(Network, self).__init__(parent)
-
-    self.controller = NetworkController()
-    self.configuration = self.controller.configuration
-
-    self.initUI()
-    self.retranslateUi()
-    self.__set_current_config_values()
-    
-
-  def initUI(self):
-
-    self.setGeometry(QtCore.QRect(10, 260, 691, 101))
-    self.setObjectName("group_box_network_check")
-
-    #NTP GROUPBOX
-    self.group_box_ntp_configuration = QtWidgets.QGroupBox(self)
-    self.group_box_ntp_configuration.setGeometry(QtCore.QRect(10, 30, 251, 51))
-
-    #NTP SERVER 
-    self.group_box_ntp_configuration.setObjectName("group_box_ntp_configuration")
-    self.ntp_server = QtWidgets.QLineEdit(self.group_box_ntp_configuration)
-    self.ntp_server.setGeometry(QtCore.QRect(20, 20, 211, 22))
-    self.ntp_server.setObjectName("ntp_server")
-
-    #NSLOOKUP GROUPBOX 
-    self.group_box_nslookup_configuration = QtWidgets.QGroupBox(self)
-    self.group_box_nslookup_configuration.setGeometry(QtCore.QRect(280, 30, 371, 51))
-    self.group_box_nslookup_configuration.setObjectName("group_box_nslookup_configuration")
-
-    #NSLOOKUP DNS SERVER 
-    self.nslookup_dns_server = QtWidgets.QLineEdit(self.group_box_nslookup_configuration)
-    self.nslookup_dns_server.setGeometry(QtCore.QRect(10, 20, 151, 22))
-    self.nslookup_dns_server.setObjectName("nslookup_dns_server")
-
-    #NSLOOKUP ENABLE TCP 
-    self.nslookup_enable_tcp = QtWidgets.QCheckBox(self.group_box_nslookup_configuration)
-    self.nslookup_enable_tcp.setGeometry(QtCore.QRect(180, 20, 81, 17))
-    palette = QtGui.QPalette()
-    brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
-    brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-    #palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
-    brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
-    brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-    #palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
-    brush = QtGui.QBrush(QtGui.QColor(120, 120, 120))
-    brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-    #palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
-    self.nslookup_enable_tcp.setPalette(palette)
-    self.nslookup_enable_tcp.setObjectName("nslookup_enable_tcp")
-
-    #NSLOOKUP ENABLE VERBOSE MODE
-    self.nslookup_enable_verbose_mode = QtWidgets.QCheckBox(self.group_box_nslookup_configuration)
-    self.nslookup_enable_verbose_mode.setGeometry(QtCore.QRect(270, 20, 81, 17))
-    palette = QtGui.QPalette()
-    brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
-    brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-    palette.setBrush(QtGui.QPalette.ColorGroup.Active,QtGui.QPalette.ColorRole.WindowText, brush)
-    brush = QtGui.QBrush(QtGui.QColor(0, 0, 0))
-    brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-    palette.setBrush(QtGui.QPalette.ColorGroup.Inactive, QtGui.QPalette.ColorRole.WindowText, brush)
-    brush = QtGui.QBrush(QtGui.QColor(120, 120, 120))
-    brush.setStyle(QtCore.Qt.BrushStyle.SolidPattern)
-    palette.setBrush(QtGui.QPalette.ColorGroup.Disabled,QtGui.QPalette.ColorRole.WindowText, brush)
-    self.nslookup_enable_verbose_mode.setPalette(palette)
-    self.nslookup_enable_verbose_mode.setChecked(True)
-    self.nslookup_enable_verbose_mode.setObjectName("nslookup_enable_verbose_mode")
-
-      
-  
-  def retranslateUi(self):
-    _translate = QtCore.QCoreApplication.translate
-    self.setTitle(_translate("ConfigurationView", "Network Check"))
-    self.group_box_ntp_configuration.setTitle(_translate("ConfigurationView", "NTP"))
-    self.group_box_nslookup_configuration.setTitle(_translate("ConfigurationView", "Nslookup"))
-    self.nslookup_dns_server.setPlaceholderText(_translate("ConfigurationView", "DNS"))
-    self.nslookup_enable_tcp.setText(_translate("ConfigurationView", "Enable TCP"))
-    self.nslookup_enable_verbose_mode.setText(_translate("ConfigurationView", "Verbose"))
-
-  
-  def __set_current_config_values(self):
-    self.ntp_server.setText(self.configuration['ntp_server'])
-    self.nslookup_dns_server.setText(self.configuration['nslookup_dns_server'])
-    self.nslookup_enable_tcp.setChecked(self.configuration['nslookup_enable_tcp'])
-    self.nslookup_enable_verbose_mode.setChecked(self.configuration['nslookup_enable_verbose_mode'])
-
-  def __get_current_values(self):
-
-      for keyword in self.configuration:
-         item = self.findChild(QtCore.QObject, keyword)
-
-         if item is not None:
-            if isinstance(item, QtWidgets.QLineEdit) is not False and item.text():
-               item = item.text()
-            elif isinstance(item, QtWidgets.QCheckBox):
-                    item = item.isChecked()
-
-            self.configuration[keyword] = item
+######
 
 
-  def accept(self) -> None:
-    self.__get_current_values()
-    self.controller.configuration = self.configuration
-  
-  def reject(self) -> None:
-    pass
+from PyQt6 import QtCore, QtWidgets
+from PyQt6.QtWidgets import QFileDialog
 
+from view.configurations.tabs.general.typesproceedings import TypesProceedings as TypesproceedingsView
+from view.configurations.tabs.general.network import Network as NetworkView
+from controller.configurations.tabs.general.general import General as GeneralController
+
+import os
+
+__is_tab__ = True
+
+
+class Options(QtWidgets.QWidget):
+
+    def __init__(self, parent=None):
+
+        super(Options, self).__init__(parent)
+
+        self.controller = GeneralController()
+        self.configuration = self.controller.configuration
+
+        self.setObjectName("configuration_option")
+
+        self.initUI()
+        self.retranslateUi()
+        self.__set_current_config_values()
+
+    def initUI(self):
+        #-----
+        self.enable_network_tools_box = QtWidgets.QGroupBox(parent=self)
+        self.enable_network_tools_box.setGeometry(QtCore.QRect(10, 20, 691, 171))
+        self.enable_network_tools_box.setObjectName("enable_network_tools_box")
+
+        self.whois = QtWidgets.QCheckBox(parent=self.enable_network_tools_box)
+        self.whois.setGeometry(QtCore.QRect(20, 20, 251, 17))
+        self.whois.setObjectName("whois")
+
+        self.delete_project_folder_3 = QtWidgets.QCheckBox(parent=self.enable_network_tools_box)
+        self.delete_project_folder_3.setGeometry(QtCore.QRect(20, 60, 391, 17))
+        self.delete_project_folder_3.setObjectName("delete_project_folder_3")
+
+        self.load_css_3 = QtWidgets.QCheckBox(parent=self.enable_network_tools_box)
+        self.load_css_3.setGeometry(QtCore.QRect(20, 80, 91, 17))
+        self.load_css_3.setChecked(True)
+        self.load_css_3.setObjectName("load_css_3")
+        self.load_images_3 = QtWidgets.QCheckBox(parent=self.enable_network_tools_box)
+        self.load_images_3.setGeometry(QtCore.QRect(20, 100, 111, 17))
+        self.load_images_3.setChecked(True)
+        self.load_images_3.setObjectName("load_images_3")
+        self.zip_project_folder_3 = QtWidgets.QCheckBox(parent=self.enable_network_tools_box)
+        self.zip_project_folder_3.setGeometry(QtCore.QRect(20, 40, 331, 17))
+        self.zip_project_folder_3.setObjectName("zip_project_folder_3")
+        self.load_javascript_3 = QtWidgets.QCheckBox(parent=self.enable_network_tools_box)
+        self.load_javascript_3.setGeometry(QtCore.QRect(20, 120, 121, 17))
+        self.load_javascript_3.setChecked(True)
+        self.load_javascript_3.setObjectName("load_javascript_3")
+
+
+        # PROCEEDINGS TYPE LIST
+        self.group_box_network_check = NetworkView(self)
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("Options", "Options"))
+        self.enable_network_tools_box.setTitle(_translate("Options", "Enable Network Check"))
+        self.whois.setText(_translate("Options", "Whois"))
+
+
+    def __select_cases_folder(self):
+        cases_folder = QtWidgets.QFileDialog.getExistingDirectory(self,
+                                                                  'Select Cases Folder',
+                                                                  os.path.expanduser(self.cases_folder.text()),
+                                                                  QFileDialog.Option.ShowDirsOnly)
+        self.cases_folder.setText(cases_folder)
+
+    def __set_current_config_values(self):
+        pass
+    def __get_current_values(self):
+
+        for keyword in self.configuration:
+            item = self.findChild(QtCore.QObject, keyword)
+
+            if item is not None:
+                if isinstance(item, QtWidgets.QComboBox) is not False and item.currentText():
+                    item = item.currentText()
+                elif isinstance(item, QtWidgets.QLineEdit) is not False and item.text():
+                    item = item.text()
+                elif isinstance(item, QtWidgets.QPlainTextEdit) is not False and item.toPlainText():
+                    item = item.toPlainText()
+
+                self.configuration[keyword] = item
+
+    def accept(self) -> None:
+        self.group_box_types_proceedings.accept()
+        self.group_box_network_check.accept()
+        self.__get_current_values()
+        self.controller.configuration = self.configuration
+
+    def reject(self) -> None:
+        pass
